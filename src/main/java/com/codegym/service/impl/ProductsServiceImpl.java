fcf030181b5634f3b08cc8a99b08d1a1ce10ee08
Lê Hoàng Thiên Phong <thienphong251199@gmail.com>
2019-10-16 21:13:36 +0700

    oke

---
+++ src/main/java/com/codegym/service/impl/ProductsServiceImpl.java
@@ -0,0 +1,22 @@
+package com.codegym.service.impl;
+
+import com.codegym.model.Product;
+import com.codegym.repository.ProductRepository;
+import com.codegym.service.ProductService;
+import org.springframework.beans.factory.annotation.Autowired;
+
+import java.util.List;
+
+public class   ProductsServiceImpl implements ProductService {
+    @Autowired
+    private ProductRepository productRepository;
+    @Override
+    public List findAll() {
+        return productRepository.findAll();
+    }
+
+    @Override
+    public Product findById(long id) {
+        return productRepository.findById(id);
+    }
+}
